791c123a359f9f1b4c3d3aa97786c8f6773fc5b0
Kate Finlay <kate.n.finlay@gmail.com>
2014-09-22 00:01:58 -0700

    Basic functionality implemented but numpadview does not show buttons

---
+++ Sudoku Grid/KFAMGridModel.m
@@ -0,0 +1,13 @@
+//
+//  KFAMGridModel.m
+//  Sudoku Grid
+//
+//  Created by Katharine Finlay on 9/20/14.
+//  Copyright (c) 2014 Kate Finlay, Alejandro Mendoza. All rights reserved.
+//
+
+#import "KFAMGridModel.h"
+
+@implementation KFAMGridModel
+
+@end
---
+++ Sudoku Grid/KFAMNumPadView.m
@@ -0,0 +1,31 @@
+//
+//  KFAMNumPadView.m
+//  Sudoku Grid
+//
+//  Created by Katharine Finlay on 9/20/14.
+//  Copyright (c) 2014 Kate Finlay, Alejandro Mendoza. All rights reserved.
+//
+
+#import "KFAMNumPadView.h"
+
+@implementation KFAMNumPadView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
