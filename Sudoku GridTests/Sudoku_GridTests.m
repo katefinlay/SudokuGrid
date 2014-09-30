8a8963cd4b1c4da613ceea276b525623346a4cad
Kate Finlay <kate.n.finlay@gmail.com>
2014-09-30 12:59:50 -0700

    Added more unit tests, all work except immutable

--- Sudoku GridTests/Sudoku_GridTests.m
+++ Sudoku GridTests/Sudoku_GridTests.m
@@ -8,9 +8,11 @@
 
 #import <XCTest/XCTest.h>
 #import "KFAMGridModel.h"
+#import "KFAMGridGenerator.h"
 
 @interface Sudoku_GridTests : XCTestCase {
     KFAMGridModel* _gridModel;
+    KFAMGridGenerator* _gridGenerator;
 }
 
 @end
@@ -23,6 +25,26 @@
     // Put setup code here. This method is called before the invocation of each test method in the class.
     _gridModel = [KFAMGridModel alloc];
     [_gridModel initialize];
+    _gridGenerator = [KFAMGridGenerator alloc];
+    [_gridGenerator initialize];
+    
+    int testGrid[81] =
+    {1,0,3,5,0,0,0,0,9,
+        0,0,2,4,6,0,0,7,0,
+        2,4,0,1,0,0,0,0,0,
+        0,0,0,0,2,6,7,0,8,
+        0,0,1,0,7,0,9,4,0,
+        0,6,4,0,1,0,0,0,2,
+        5,0,0,9,0,1,6,0,0,
+        0,0,0,0,5,4,0,1,0,
+        7,3,0,0,0,0,0,0,0};
+    for (int r = 0; r < 9; r++) {
+        for (int c = 0; c < 9; c++) {
+            [_gridModel inputNumber:testGrid[(r*9 + c)] atRow:r andCol:c];
+        }
+    }
+    
+    
 }
 
 - (void)tearDown
@@ -61,24 +83,58 @@
 }
 
 
-//- (void)getGrid:(NSMutableArray*) grid {
-//    [_gridModel getGrid:_grid];
-//    
-//}
-
-- (void)getNewGrid:(NSMutableArray*) grid {
+- (void)test_generateNewGrid {
+    [_gridGenerator generateNewGrid];
+    int initialGrid[81];
+    for (int r = 0; r < 9; r++) {
+        for (int c = 0; c < 9; c++) {
+            initialGrid[r*9 + c] = [_gridGenerator getNumberWithRow:r andCol:c];
+        }
+    }
+    [_gridGenerator generateNewGrid];
+    BOOL gridChanged = false;
+    for (int r = 0; r < 9; r++) {
+        for (int c = 0; c < 9; c++) {
+            int initNum = initialGrid[r*9 + c];
+            int newNum = [_gridGenerator getNumberWithRow:r andCol:c];
+            NSLog(@"init num %d newNum %d", initNum, newNum);
+            if (initNum != newNum) {
+                gridChanged = true;
+            }
+        }
+    }
+    XCTAssert(gridChanged, @"Checking that generate new grid changes the grid");
     
 }
 
-- (void)makeNewGame {
+- (void)test_makeNewGame {
+    [_gridModel makeNewGame];
+    int initialGrid[81];
+    for (int r = 0; r < 9; r++) {
+        for (int c = 0; c < 9; c++) {
+            initialGrid[r*9 + c] = [_gridModel getNumberWithRow:r andCol:c];
+        }
+    }
     
+    [_gridModel makeNewGame];
+    
+    BOOL gridChanged = false;
+    for (int r = 0; r < 9; r++) {
+        for (int c = 0; c < 9; c++) {
+            if (initialGrid[r*9 + c] != [_gridModel getNumberWithRow:r andCol:c]) {
+                gridChanged = true;
+            }
+        }
+    }
+    XCTAssert(gridChanged, @"Checking that generate new grid changes the grid");
 }
 
-//-(BOOL)checkSolution {
-//    
-//}
-
-
+- (void) test_checkSolution {
+    for (int i = 0; i < 81; i++) {
+        [_gridModel inputNumber:1 atRow:i/9 andCol:i%9];
+    }
+    XCTAssert([_gridModel checkSolution], @"checking that filling the grid will cause it to behave as expected, to identify it as a checked solution"); //if ismutable and isvalid checks work, then we only need to check if the grid is full to see if the grid is solved.
+}
 
 
 @end
